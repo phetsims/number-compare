71fe454d033a4ed1e7f085af7ba2facc7840608c
chrisklus <christopher.klusendorf@colorado.edu>
2022-10-21 14:46:46 -0600

    Disable preference controls if associated screen doesn't exist, show all_html link if only one locale, convert compareMax and subitizeTime controls to number spinners, see https://github.com/phetsims/number-play/issues/189

--- js/common/view/NumberComparePreferencesNode.ts
+++ js/common/view/NumberComparePreferencesNode.ts
@@ -7,29 +7,65 @@
  * @author Chris Klusendorf (PhET Interactive Simulations)
  */
 
-import { HBox, Text } from '../../../../scenery/js/imports.js';
-import HSlider from '../../../../sun/js/HSlider.js';
-import Utils from '../../../../dot/js/Utils.js';
+import { Font, HBox, Text, VBox } from '../../../../scenery/js/imports.js';
+import Range from '../../../../dot/js/Range.js';
 import numberCompare from '../../numberCompare.js';
 import numberComparePreferences, { NumberComparePreferences } from '../model/numberComparePreferences.js';
 import NumberSuiteCommonPreferencesNode from '../../../../number-play/js/common/view/NumberSuiteCommonPreferencesNode.js';
 import NumberCompareStrings from '../../NumberCompareStrings.js';
+import Property from '../../../../axon/js/Property.js';
+import NumberSpinner from '../../../../sun/js/NumberSpinner.js';
+import NumberPlayConstants from '../../../../number-play/js/common/NumberPlayConstants.js';
 
 export default class NumberComparePreferencesNode extends NumberSuiteCommonPreferencesNode<NumberComparePreferences> {
 
   public constructor() {
 
+    // TODO: factor out type with subitize time control if kept
     const compareMaxText = new Text( NumberCompareStrings.maximumTotalStringProperty,
-      NumberSuiteCommonPreferencesNode.CONTROL_TEXT_OPTIONS );
-    const compareMaxSlider = new HSlider( numberComparePreferences.compareMaxProperty,
-      numberComparePreferences.compareMaxProperty.range!, {
-        constrainValue: ( value: number ) => Utils.toFixedNumber( value, 1 )
+      NumberSuiteCommonPreferencesNode.CONTROL_TEXT_BOLD_OPTIONS );
+    const compareMaxSpinner = new NumberSpinner( numberComparePreferences.compareMaxProperty,
+      new Property<Range>( new Range( 1, NumberPlayConstants.TWENTY ) ), {
+        arrowsPosition: 'leftRight',
+        deltaValue: 1,
+        numberDisplayOptions: {
+          valuePattern: '{{value}}',
+          decimalPlaces: 0,
+          align: 'center',
+          xMargin: 10,
+          yMargin: 3,
+          textOptions: {
+            font: new Font( { size: NumberSuiteCommonPreferencesNode.FONT_SIZE } )
+          }
+        }
       } );
-    const compareMaxControl = new HBox( {
-      children: [ compareMaxText, compareMaxSlider ]
+    const compareMaxHBox = new HBox( {
+      children: [ compareMaxText, compareMaxSpinner ],
+      spacing: 105
+    } );
+
+    const compareMaxDescriptionText = new Text( 'Set the largest number that can be compared.',
+      NumberSuiteCommonPreferencesNode.CONTROL_TEXT_OPTIONS );
+    const compareMaxControl = new VBox( {
+      children: [ compareMaxHBox, compareMaxDescriptionText ],
+      spacing: NumberSuiteCommonPreferencesNode.CONTROL_DESCRIPTION_SPACING,
+      align: 'left'
     } );
 
     super( numberComparePreferences, [ compareMaxControl ] );
+
+    // disable any controls that are not applicable to the current selection of screens
+    if ( QueryStringMachine.containsKey( 'screens' ) ) {
+      const screens = phet.chipper.queryParameters.screens;
+      const isSecondLocaleScreen = screens.includes( 1 );
+      const isCompareScreen = screens.includes( 1 );
+      const isLabScreen = screens.includes( 2 );
+
+      this.readAloudToggleSwitch.enabled = isSecondLocaleScreen;
+      this.showSecondLocaleControl.enabled = isSecondLocaleScreen;
+      compareMaxControl.enabled = isCompareScreen;
+      this.showLabOnesToggleSwitch.enabled = isLabScreen;
+    }
   }
 }
 
