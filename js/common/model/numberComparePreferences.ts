e1ac14bd7880ee8dcc6793fc32affeecbfa9e270
chrisklus <christopher.klusendorf@colorado.edu>
2022-11-01 00:28:09 -0600

    Omit compareMax from preferences dialog, see https://github.com/phetsims/number-play/issues/189

--- js/common/model/numberComparePreferences.ts
+++ js/common/model/numberComparePreferences.ts
@@ -7,25 +7,9 @@
  * @author Chris Klusendorf (PhET Interactive Simulations)
  */
 
-import NumberPlayQueryParameters from '../../../../number-play/js/common/NumberPlayQueryParameters.js';
-import NumberProperty from '../../../../axon/js/NumberProperty.js';
-import Range from '../../../../dot/js/Range.js';
 import numberCompare from '../../numberCompare.js';
 import NumberSuiteCommonPreferences from '../../../../number-play/js/common/model/NumberSuiteCommonPreferences.js';
-import NumberPlayConstants from '../../../../number-play/js/common/NumberPlayConstants.js';
 
-export class NumberComparePreferences extends NumberSuiteCommonPreferences {
-  public readonly compareMaxProperty: NumberProperty;
-
-  public constructor() {
-    super();
-
-    this.compareMaxProperty = new NumberProperty( NumberPlayQueryParameters.compareMax, {
-      range: new Range( 0, NumberPlayConstants.TWENTY )
-    } );
-  }
-}
-
-const numberPlayPreferences = new NumberComparePreferences();
-numberCompare.register( 'numberPlayPreferences', numberPlayPreferences );
-export default numberPlayPreferences;
+const numberComparePreferences = new NumberSuiteCommonPreferences();
+numberCompare.register( 'numberComparePreferences', numberComparePreferences );
+export default numberComparePreferences;
--- js/common/view/NumberComparePreferencesNode.ts
+++ js/common/view/NumberComparePreferencesNode.ts
@@ -7,62 +7,24 @@
  * @author Chris Klusendorf (PhET Interactive Simulations)
  */
 
-import { Font, HBox, Text, VBox } from '../../../../scenery/js/imports.js';
-import Range from '../../../../dot/js/Range.js';
 import numberCompare from '../../numberCompare.js';
-import numberComparePreferences, { NumberComparePreferences } from '../model/numberComparePreferences.js';
+import numberComparePreferences from '../model/numberComparePreferences.js';
 import NumberSuiteCommonPreferencesNode from '../../../../number-play/js/common/view/NumberSuiteCommonPreferencesNode.js';
-import NumberCompareStrings from '../../NumberCompareStrings.js';
-import Property from '../../../../axon/js/Property.js';
-import NumberSpinner from '../../../../sun/js/NumberSpinner.js';
-import NumberPlayConstants from '../../../../number-play/js/common/NumberPlayConstants.js';
+import NumberSuiteCommonPreferences from '../../../../number-play/js/common/model/NumberSuiteCommonPreferences.js';
 
-export default class NumberComparePreferencesNode extends NumberSuiteCommonPreferencesNode<NumberComparePreferences> {
+export default class NumberComparePreferencesNode extends NumberSuiteCommonPreferencesNode<NumberSuiteCommonPreferences> {
 
   public constructor() {
 
-    // TODO: factor out type with subitize time control if kept
-    const compareMaxText = new Text( NumberCompareStrings.maximumTotalStringProperty,
-      NumberSuiteCommonPreferencesNode.CONTROL_TEXT_BOLD_OPTIONS );
-    const compareMaxSpinner = new NumberSpinner( numberComparePreferences.compareMaxProperty,
-      new Property<Range>( new Range( 1, NumberPlayConstants.TWENTY ) ), {
-        arrowsPosition: 'leftRight',
-        deltaValue: 1,
-        numberDisplayOptions: {
-          valuePattern: '{{value}}',
-          decimalPlaces: 0,
-          align: 'center',
-          xMargin: 10,
-          yMargin: 3,
-          textOptions: {
-            font: new Font( { size: NumberSuiteCommonPreferencesNode.FONT_SIZE } )
-          }
-        }
-      } );
-    const compareMaxHBox = new HBox( {
-      children: [ compareMaxText, compareMaxSpinner ],
-      spacing: 105
-    } );
-
-    const compareMaxDescriptionText = new Text( 'Set the largest number that can be compared.',
-      NumberSuiteCommonPreferencesNode.CONTROL_TEXT_OPTIONS );
-    const compareMaxControl = new VBox( {
-      children: [ compareMaxHBox, compareMaxDescriptionText ],
-      spacing: NumberSuiteCommonPreferencesNode.CONTROL_DESCRIPTION_SPACING,
-      align: 'left'
-    } );
-
-    super( numberComparePreferences, [ compareMaxControl ] );
+    super( numberComparePreferences, [] );
 
     // disable any controls that are not applicable to the current selection of screens
     if ( QueryStringMachine.containsKey( 'screens' ) ) {
       const screens = phet.chipper.queryParameters.screens;
       const isSecondLocaleScreen = screens.includes( 1 );
-      const isCompareScreen = screens.includes( 1 );
       const isLabScreen = screens.includes( 2 );
 
       this.showSecondLocaleControl.enabled = isSecondLocaleScreen;
-      compareMaxControl.enabled = isCompareScreen;
       this.showLabOnesToggleSwitch.enabled = isLabScreen;
     }
   }
