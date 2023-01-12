0a347859abba4f8372d6066e38897f19a2dad049
phet-dev <phetadmin@gmail.com>
2023-01-12 04:50:54 -0700

    update copyright dates from daily grunt work

--- js/common/view/NumberComparePreferencesNode.ts
+++ js/common/view/NumberComparePreferencesNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * NumberComparePreferencesNode is the user interface for sim-specific preferences for Number Compare, accessed via the
