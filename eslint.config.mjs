8e8c98d7b62a2d8116223c33225e7d94df85d65f
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:05 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for number-compare.
+ * ESLint configuration for number-compare.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
