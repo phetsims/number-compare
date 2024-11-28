8dbafaa92ec31bb8322d06c9181e7d22a7c9a440
phet-dev <phetadmin@gmail.com>
2024-11-28 03:41:09 -0700

    update copyright dates from daily grunt work

--- js/compare/view/ComparisonTextNode.ts
+++ js/compare/view/ComparisonTextNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2023, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 /**
  * Text for displaying a comparison statement for the two current numbers.
