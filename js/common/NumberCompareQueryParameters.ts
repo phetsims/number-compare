56a74c88e1c50eacb625f04f74642580be5b7c91
phet-dev <phetadmin@gmail.com>
2026-03-20 10:03:25 -0600

    update copyright dates from daily grunt work

--- js/common/NumberCompareQueryParameters.ts
+++ js/common/NumberCompareQueryParameters.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2025, University of Colorado Boulder
+// Copyright 2023-2026, University of Colorado Boulder
 
 /**
  * NumberCompareQueryParameters defines query parameters that are specific to this simulation.
